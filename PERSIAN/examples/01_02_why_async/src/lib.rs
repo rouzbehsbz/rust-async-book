5e09739c29cac3899b00fdf424cfe08f5d780d49
rouzbehsbz <rouzbehsbz@gmail.com>
2023-01-06 19:17:53 +0330

    complete translation fo chapter 1 section 3

--- PERSIAN/examples/01_02_why_async/src/lib.rs
+++ PERSIAN/examples/01_02_why_async/src/lib.rs
@@ -10,11 +10,11 @@ fn download(_url: &str) {
 #[test]
 // ANCHOR: get_two_sites
 fn get_two_sites() {
-    // ایجاد دو تا thread جدید که کار دانلود رو انجام بده
+    // ایجاد دو تا ترد جدید که کار دانلود رو انجام بده.
     let thread_one = thread::spawn(|| download("https://www.foo.com"));
     let thread_two = thread::spawn(|| download("https://www.bar.com"));
 
-    // صبر میکنیم تا هر دو تا thread کارشون تموم شه
+    // صبر میکنیم تا هردو ترد کارشون تموم شه
     thread_one.join().expect("thread one panicked");
     thread_two.join().expect("thread two panicked");
 }
@@ -26,12 +26,12 @@ async fn download_async(_url: &str) {
 
 // ANCHOR: get_two_sites_async
 async fn get_two_sites_async() {
-    // ایجاد دو تا Future جدید که پس از تموم شدن کارشون
+    // ایجاد دو تا فیوچر جدید که پس از تموم شدن کارشون
     // صفحات وب رو به صورت ناهمزمان دانلود میکنن
     let future_one = download_async("https://www.foo.com");
     let future_two = download_async("https://www.bar.com");
 
-    // هر دو Future رو اجرا میکنیم که در یک زمان کارشون تموم شه
+    // هر دو فیوچر رو اجرا میکنیم که در یک زمان کارشون تموم شه
     join!(future_one, future_two);
 }
 // ANCHOR_END: get_two_sites_async
