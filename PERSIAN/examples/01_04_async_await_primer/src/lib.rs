be788806274d210ee1310976f95a6e9a069c6a13
rouzbehsbz <rouzbehsbz@gmail.com>
2023-01-07 20:17:51 +0330

    add translation for example number 01_04 and fix LTR mode in codes section

--- PERSIAN/examples/01_04_async_await_primer/src/lib.rs
+++ PERSIAN/examples/01_04_async_await_primer/src/lib.rs
@@ -4,9 +4,10 @@ use futures::executor::block_on;
 
 mod first {
 // ANCHOR: hello_world
-// `block_on` blocks the current thread until the provided future has run to
-// completion. Other executors provide more complex behavior, like scheduling
-// multiple futures onto the same thread.
+// تابع "بلاک آن" تردی که در آن اجرا می شود را بلاک و مسدود میکند
+// و وقتی آزاد می شود که اون "فیوچر"ی که در حال اجرای آن است به پایان برسد
+// اجرا کننده های دیگه رفتار های پیچیده تری از خودشون نشون میدن
+// مثل زمان بندی برای اجرای چند فیوچر در یک ترد
 use futures::executor::block_on;
 
 async fn hello_world() {
@@ -14,8 +15,8 @@ async fn hello_world() {
 }
 
 fn main() {
-    let future = hello_world(); // Nothing is printed
-    block_on(future); // `future` is run and "hello, world!" is printed
+    let future = hello_world(); // اینجا هیچی پرینت نمیشه
+    block_on(future); // تابع "فیوچر" اجرا شد و متن بالا پرینت شد
 }
 // ANCHOR_END: hello_world
 
@@ -46,9 +47,10 @@ mod third {
 use super::*;
 // ANCHOR: block_on_main
 async fn learn_and_sing() {
-    // Wait until the song has been learned before singing it.
-    // We use `.await` here rather than `block_on` to prevent blocking the
-    // thread, which makes it possible to `dance` at the same time.
+    // اینجا اول صبر میکنیم آهنگ یاد گرفته بشه قبل از خوندنش
+    // انیجا ما از "اویت" به جای "بلاک آن" استفاده میکنیم تا جلوگیری
+    // بشه از بلاک شدن و مسدود شدن ترد اینجوری میتونیم همزمان با این کار
+    // تابع رقصیدن هم اجرا کنیم
     let song = learn_song().await;
     sing_song(song).await;
 }
@@ -57,11 +59,11 @@ async fn async_main() {
     let f1 = learn_and_sing();
     let f2 = dance();
 
-    // `join!` is like `.await` but can wait for multiple futures concurrently.
-    // If we're temporarily blocked in the `learn_and_sing` future, the `dance`
-    // future will take over the current thread. If `dance` becomes blocked,
-    // `learn_and_sing` can take back over. If both futures are blocked, then
-    // `async_main` is blocked and will yield to the executor.
+    // ماکرو "جوین" مثل "اویت" هست. با استفاده از این ماکرو میشه
+    // برای تموم شدن چندین "فیوچر" به صورت همزمان صبر کرد.
+    // اگه به صورت موقت تابع اولی ترد رو بخواد مسدود کنه کار تابع بعدی یعنی رقصیدن انجام میشه.
+    // و برعکسش هم میتونه اتفاق بیفته، یعنی اگه رقصیدن بخواد مسدود بشه اون یکی تابع اجرا میشه
+    // و اگه جفتشون بلاک بشن این فانکشن کلا بلاک میشه و اجراکنندش صبر میکنه تموم بشه کارش
     futures::join!(f1, f2);
 }
 
